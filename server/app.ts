0f63a3f95e1f20502b03c94f1a357c7c436592c7
Tracy Ngot <tracy.ngot@gmail.com>
2020-03-20 18:48:28 +0000

    core(netlify): add @babel/polyfill

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,4 @@
+import '@babel/polyfill/noConflict';
 import serverless from 'serverless-http';
 import server from './utils/server';
 
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import '@babel/polyfill/noConflict';
 import server from './utils/server';
 
 server.start(4000);
